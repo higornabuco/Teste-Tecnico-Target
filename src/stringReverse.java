520f3cf20dd4957aad9f281ba18ad6da47029908
Higor Nabuco <58661589+higornabuco@users.noreply.github.com>
2024-09-15 16:43:18 +0000

    introdução de comentarios nos arquivos 'PercentualFaturamento' e 'stringReverse'

--- src/stringReverse.java
+++ src/stringReverse.java
@@ -1,3 +1,12 @@
+/*
+Escreva um programa que inverta os caracteres de um string.
+
+IMPORTANTE:
+a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+b) Evite usar funções prontas, como, por exemplo, reverse;
+ */
+
+
 public class stringReverse {
     public static void main(String[] args) {
         String original = "exemplo";
